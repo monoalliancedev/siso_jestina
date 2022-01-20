1c993c7ec2dc8349eb17c39d6fe70a8ae8b32b57
DESKTOP-503KA6A\TeamLeader <mono@monoalliance.com>
2022-01-20 10:45:59 +0900

    RECRUIT >직무소개 수정

--- src/main/webapp/html/scripts/recruit.js
+++ src/main/webapp/html/scripts/recruit.js
@@ -1,71 +1,44 @@
 $(document).ready(function () {
     /*----------직무소개----------*/
-    //Tab
-    $('.recruit.duty .pcTab a').click(function(){
-        $('.recruit.duty .pcTab a').removeClass('on')
-        $('.recruit.duty .tabData').hide();
-        $($(this).attr('href')).show();
-        $(this).addClass('on');
-        return false;
-    })
-    
-    function TabDataResize(){
-        if($(window).width() < 813){
-            $('.tabDataWrap > div ').flexslider({
-                animation: '',
-                directionNav: false,
-                animationLoop: false,
-                controlNav: true,
-                controlsContainer: ".tabCtrl",
-                slideshow: false,
-                start: function () {
-                    function ctrlAction() {
-                        var tabPaging = $('.tabDataWrap .tabData').length;
-                        for (var i = 0; i < tabPaging; i++) {
-                            var tabArr = $('.tabDataWrap .tabData').eq(i).attr('class');
-                            var tabText = tabArr.split("tabData").pop('').split(' ')[1];
-                            $('.flex-control-paging li').eq(i).find('span').html(tabText);
-                        }
-                        var tabDataHT = $('.tabDataWrap .tabData.flex-active-slide').outerHeight();
-                        $('.duty .tabDataWrap > div').css('height' , tabDataHT);
-                    };
-                    ctrlAction();
-                },
-                after: function () {
-                    var tabDataHT = $('.tabDataWrap .tabData.flex-active-slide').outerHeight();
-                    $('.duty .tabDataWrap > div').css('height' , tabDataHT);
-                },
-            });
-        }
+    function dutyFun() {
+        $('.duty .tabDataWrap').flexslider({
+            animation: '',
+            directionNav: false,
+            animationLoop: false,
+            controlNav: true,
+            slideshow: false,
+            smoothHeight: true,
+            controlsContainer: '.duty .tabButton .buttons',
+            start: function () {
+                var tabEA = $('.tabDataWrap .tabData').length;
+                for (var i = 0; i < tabEA; i++) {
+                    var tabTitle = $('.tabDataWrap .tabData').eq(i).attr('data-tab');
+                    $('.tabButton ol li').eq(i).find('span').html(tabTitle);
+                }
+                $('.duty .tabButton ol li').each(function () {
+                    $(this).next('li').css({ 'left': $(this).outerWidth() + parseInt($(this).css('left')) })
+                });
+            },
+            after: function () {
+                var prevOffset = $('.duty .tabButton ol a.flex-active').parent('li').css('left');
+                $('.duty .tabButton .buttons ol').animate({ scrollLeft: prevOffset });
+            }
+        });
     }
-    TabDataResize();
-    $(window).resize(function(){
-        TabDataResize();
+    dutyFun();
+    $('.duty .tabButton > a.prev').click(function () {
+        $('.duty .tabDataWrap').flexslider('prev');
+        return false;
+    });
+    $('.duty .tabButton > a.next').click(function () {
+        $('.duty .tabDataWrap').flexslider('next');
+        return false;
+    });
+    $(window).resize(function () {
+        $('.duty .tabButton ol li').each(function () {
+            $(this).next('li').css({ 'left': $(this).outerWidth() + parseInt($(this).css('left')) })
+        })
     })
-    $('.recruit.duty .tabCtrl .prev').click(function () {
-        if($('.duty .tabCtrl ol li:first-child a').hasClass('flex-active') == true){
-            $('.duty .tabDataWrap > div').flexslider('stop');
-        }else{
-            var tabOffset = $('.duty .tabCtrl .flex-active').offset().left;
-            $('.duty .tabCtrl ol').stop().animate({'scrollLeft' : -tabOffset})
-
-            $('.duty .tabDataWrap > div').flexslider('prev');
-        }
-        return false;
-    });
-
-    $('.recruit.duty .tabCtrl .next').click(function () {
-        if($('.duty .tabCtrl ol li:nth-child(6) a').hasClass('flex-active') == true){
-            $('.duty .tabDataWrap > div').flexslider('stop');
-        }else{
-            var tabOffset = $('.duty .tabCtrl .flex-active').offset().left;
-            $('.duty .tabCtrl ol').stop().animate({'scrollLeft' : tabOffset})
-               
-            $('.duty .tabDataWrap > div').flexslider('next');
-        }
-        return false;
-    });
-
     /*----------채용공고----------*/
     //목록 제목
     $('.recruit.jobOpening .listWrap li.tit').each(function () {
