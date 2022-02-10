3e1dd07baa456ed8be398a2399ffaa1b835ed5e6
DESKTOP-503KA6A\TeamLeader <mono@monoalliance.com>
2022-02-10 10:06:49 +0900

    [오류조치 NO.31]FAQ 해당 행(버튼) 클릭 시 해당 질문에 대한 답이 노출되지 않습니다. 수정

--- src/main/webapp/scripts/recruit.js
+++ src/main/webapp/scripts/recruit.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    /*----------채용공고----------*/
+    //FAQ
+    $('.recruit.jobOpening .faqWrap dt').click(function () {
+        if ($(this).hasClass('on')) {
+            $(this).removeClass('on');
+            $(this).next('dd').stop().slideUp();
+            $(this).next('dd').scrollTop(0);
+        } else {
+            $('.jobOpening .faqWrap dt').removeClass('on');
+            $('.jobOpening .faqWrap dd').stop().slideUp();
+            $(this).next('dd').stop().slideDown();
+            $(this).addClass('on');
+        }
+    });
+    //목록 제목
+    $('.recruit.jobOpening .listWrap li.tit').each(function () {
+        var length = 25;
+        $(this).each(function () {
+            if ($(this).text().length >= length) {
+                $(this).text($(this).text().substr(0, length) + '...');
+            }
+        });
+    });
     /*----------직무소개----------*/
     function dutyFun() {
         $('.duty .tabDataWrap').flexslider({
@@ -38,29 +61,6 @@ $(document).ready(function () {
         $('.duty .tabButton ol li').each(function () {
             $(this).next('li').css({ 'left': $(this).outerWidth() + parseInt($(this).css('left')) })
         })
-    })
-    /*----------채용공고----------*/
-    //목록 제목
-    $('.recruit.jobOpening .listWrap li.tit').each(function () {
-        var length = 25;
-        $(this).each(function () {
-            if ($(this).text().length >= length) {
-                $(this).text($(this).text().substr(0, length) + '...');
-            }
-        });
-    });
-    //FAQ
-    $('.recruit.jobOpening .faqWrap dt').click(function () {
-        if ($(this).hasClass('on')) {
-            $(this).removeClass('on');
-            $(this).next('dd').stop().slideUp();
-            $(this).next('dd').scrollTop(0);
-        } else {
-            $('.jobOpening .faqWrap dt').removeClass('on');
-            $('.jobOpening .faqWrap dd').stop().slideUp();
-            $(this).next('dd').stop().slideDown();
-            $(this).addClass('on');
-        }
     });
     /*----------입사지원----------*/
     //첨부파일
